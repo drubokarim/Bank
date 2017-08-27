d799b4aca3f3f7af07f45c5d7fdd3537cbf43777
drubokarim <drubokarim@outlook.com>
2017-08-28 00:33:09 +0600

    Add files via upload

--- ChangePassword.php
+++ ChangePassword.php
@@ -31,7 +31,53 @@ if(!isset($_SESSION["user"]))
 </fieldset>
 </div>
 </form>
-    
+    <?php
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+      $oldpass=md5(test_input($_POST["oldpassword"]));
+      $pass=test_input($_POST["newpassword"]);
+      $cpass=test_input($_POST["c_newpassword"]);
+      if($pass==$cpass)
+      {
+        $con=mysqli_connect("localhost","ASD","123");
+        if(!$con)
+      {
+        echo "<script type='text/javascript'>alert('error!couldnot connect')</script>";
+      }
+      elseif(mysqli_select_db($con,"bank"))
+     {
+           $ret="select password from user where username='".$_SESSION["user"]."'";
+           $qu=mysqli_query($con,$ret);
+           if()
+           {
+            if(mysqli_query($con,$ret))
+           {
+              $sql="update user set password='".md5($pass)."' where username='".$_SESSION["user"]."'";
+              $result=mysqli_query($con,$sql);
+              if($result)
+              {
+                echo "<script type='text/javascript'>alert('UPDATED PASSWORD SUCCESSFULLY')</script>";
+                header("Location: http://localhost/wt/login.php");
+             }
+             else
+            { 
+               echo "<script type='text/javascript'>alert('error!couldnot update password')</script>";
+            }
+           }
+         }
+           
+    }
+    else
+    {
+      echo "<script type='text/javascript'>alert('error!couldnot connect to DB')</script>";
+    }
+      }
+      else
+      {
+           echo "<script type='text/javascript'>alert('password doesnot match')</script>";
+      }
+    }
+    ?>
     <div class="Pos">
     <form method="post" action=""> 
     <fieldset class="Pos">
@@ -41,17 +87,17 @@ if(!isset($_SESSION["user"]))
             <p>
                <label>*Old Password : 
                </label>
-               <input type="text" name= "oldpassword" class="long"/>
+               <input type="password" name= "oldpassword" required>
             </p>		
 			<p>
                <label>*New Password : 
                </label>
-               <input type="text" name= "newpassword" class="long"/>
+               <input type="password" name= "newpassword" required="">
             </p>
 			<p>
                <label>*Confirm New Password : 
                </label>
-               <input type="text" name= "newpassword" class="long"/>
+               <input type="password" name= "c_newpassword" required="">
             </p>
 			<p>
 			<p>Please keep the password complicated atleast eight character.</p> 
